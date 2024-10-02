9ecf96d5f6c04a04896700ff421134356e6302b0
Daniel Romário Ferreira da Rosa <57119617+Daniel-Rosa1@users.noreply.github.com>
2024-10-01 22:31:53 -0300

    create component credit-card

---
+++ src/app/components/credit-card/credit-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credt-card',
+  templateUrl: './credit-card.component.html',
+  styleUrl: './credit-card.component.scss'
+})
+export class CreditCardComponent {
+}
