a6c5a8481d23ca3884a74665e15101e65467287a
Daniel Romário Ferreira da Rosa <57119617+Daniel-Rosa1@users.noreply.github.com>
2024-10-03 11:14:58 -0300

    create component debit-card

---
+++ src/app/components/debit-card/debit-card.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-debit-card',
+  templateUrl: './debit-card.component.html',
+  styleUrl: './debit-card.component.scss'
+})
+export class DebitCardComponent {
+
+
+}
